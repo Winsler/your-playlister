7c81c3d0f50eea8346ca6bcc7cc6885c1468f52a
Roman Makretki <r.makretski@gmail.com>
2019-12-22 01:16:49 +0300

    ref: change src dir structure

---
+++ src/components/app.tsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import Header from './header';
+
+const App: React.FC = () => (
+  <Header />
+);
+
+export default App;
---
+++ src/components/header/header.tsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const Header: React.FC = () => (
+  <div>Hallo</div>
+);
+
+export default Header;
